620e120b4627c6000aef82745cc4279a0bb771bc
Rick Huynh <Rick.Huynh@ibm.com>
2019-07-12 08:35:48 -0500

    added prep

--- src/ScrapeTweet.js
+++ src/ScrapeTweet.js
@@ -1,13 +1,5 @@
 import React from 'react';
 import { Button } from 'react-bootstrap'
-const Twitter = require('twitter-node-client').Twitter;
-
-const config = {
-    'consumerKey': "CNhkRnj2G4VBfxNS2muTLVHlA",
-    'consumerSecret': "eGwDdvcvZIhQWMpElPNlBZICIwzfPWG8DNFfIoWNmIhyNtN0mY",
-    'accessToken': "2314802384-1DChgFJp5CdAKqIIatRMEJQHCw9AGMIg6evKZ6v",
-    'accessTokenSecret': "9NwYMXa1Ted6QgKfbdQWSKLzvZRxUmHLwqOsqgX3dGVnl"
-};
 
 class ScrapeTweet extends React.Component {
   state = {
